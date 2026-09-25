5fc56c75b6f2c7f93a2489cb0cece4bee1affa59
jeancharlescoti-CYRNEA <jeancharles.coti@gmail.com>
2026-09-25 08:45:40 +0200

    V53 MAJ: actualise cache PWA

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='cyrnea-assistant-v53-20260923';
+const CACHE='cyrnea-assistant-v53-20260925-tally';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
